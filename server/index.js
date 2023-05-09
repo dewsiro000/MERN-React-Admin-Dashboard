f2ae51cd0cffdbad05e1f26552b777b8e3be6b21
dewsiro000 <norawit1267@gmail.com>
2023-05-09 14:31:20 +0700

    setup backend

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import clientRoutes from '/routes/client.js'
-import generalRoutes from '/routes/general.js'
-import managementRoutes from '/routes/management.js'
-import salesRoutes from '/routes/sales.js'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 /* CONFIGUARTION */
 dotenv.config();
@@ -24,8 +24,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 // CORS เป็นกลไกที่ web browser ใช้เวลาที่ client ส่ง request ไปยัง server ที่มี domain ต่างกัน
 
+
 /* ROUTES */
 app.use("/client", clientRoutes)
 app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
+
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
